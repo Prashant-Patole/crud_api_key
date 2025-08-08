1aea0a279a75aba8928a9ad0dcffec8120832f94
Prashant-Patole <pbaap09@gmail.com>
2025-08-08 10:34:58 +0530

    Remove all files

--- Post_Content/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from database.database import Base, engine
-from routers import post
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="Content Post API with NeonDB")
-
-app.include_router(post.router)
